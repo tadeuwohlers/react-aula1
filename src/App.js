6ed9603bf256248961049089258c721d4a8603a3
Tadeu Wohlers <tadeuwohlers@outlook.com>
2020-07-29 22:57:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+
+class Contador extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            contador: 0
+        };
+
+        this.aumentar = this.aumentar.bind(this);
+        this.diminuir = this.diminuir.bind(this);
+    }
+    
+    aumentar(){        
+        let state = this.state;
+        state.contador += 1;
+        this.setState(state)
+    }
+
+    diminuir(){        
+        let state = this.state;
+        state.contador -= 1;
+        this.setState(state)
+    }
+
+    render() {
+        return(
+        <div>
+            <button onClick={this.diminuir}>-</button>
+                {this.state.contador}
+            <button onClick={this.aumentar}>+</button>
+        </div>        
+        );
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Contador</h1>
+            <Contador />
+        </div>
+    );
 }
 
 export default App;
